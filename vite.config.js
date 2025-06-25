651c013b3683e9e8aec0d96d321db9ef743131a0
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2025-06-25 22:56:56 +0700

    update(config): allowed ngrok hosts

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    allowedHosts: ['815a-103-171-165-204.ngrok-free.app'],
+    host: true, // Penting biar bisa diakses dari luar localhost
+  },
 })
+
+
